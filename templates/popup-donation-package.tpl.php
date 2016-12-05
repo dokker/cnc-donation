3ba3a9afd2f7e34be763f709ff81b9f384b54052
docker <dock3r@gmail.com>
2016-12-05 11:08:40 +0100

    Improve donation popup frontend style

--- templates/popup-donation-package.tpl.php
+++ templates/popup-donation-package.tpl.php
@@ -8,29 +8,29 @@
     <div class="terms"><?php echo $terms; ?></div>
     <div class="mc-field-group form-group">
       <label class="sr-only" for="mce-MMERGE3"><?php _e('Name', 'sage'); ?> </label>
-      <input type="text" value="" name="MMERGE3" class="form-control form-control-lg" id="mce-MMERGE3" placeholder="<?php _e('Name'); ?>">
-      <div class="feedback"><?php _e('Missing name', 'sage'); ?></div>
+      <input type="text" value="" name="MMERGE3" class="form-control input-lg" id="mce-MMERGE3" placeholder="<?php _e('Name'); ?>">
+      <div class="help-block"><?php _e('Missing name', 'sage'); ?></div>
     </div>
     <div class="mc-field-group form-group">
       <label class="sr-only" for="mce-EMAIL"><?php _e('Email', 'sage'); ?> </label>
-      <input type="email" value="" name="EMAIL" class="required email form-control form-control-lg" id="mce-EMAIL" placeholder="<?php _e('Email'); ?>">
-      <div class="feedback"><?php _e('Missing Email', 'sage'); ?></div>
+      <input type="email" value="" name="EMAIL" class="required email form-control input-lg" id="mce-EMAIL" placeholder="<?php _e('Email'); ?>">
+      <div class="help-block"><?php _e('Missing Email', 'sage'); ?></div>
     </div>
-    <div class="mc-field-group form-group">
+    <div class="mc-field-group form-group text-left">
       <label class="custom-control custom-checkbox">
         <input type="checkbox" class="custom-control-input">
         <span class="custom-control-indicator"></span>
         <span class="custom-control-description">I would like to sign up for the Transparency Newsletter</span>
       </label>
     </div>
-    <div class="mc-field-group form-group">
+    <div class="mc-field-group form-group text-left">
       <label class="custom-control custom-checkbox">
         <input type="checkbox" class="custom-control-input">
         <span class="custom-control-indicator"></span>
         <span class="custom-control-description">I accept the terms and conditions</span>
       </label>
     </div>
-    <div class="clear">
+    <div class="clear form-group-submit">
       <input type="hidden" name="cnc-package-id">
       <input type="submit" value="<?php _e('I support Transparency International Hungary!', 'sage'); ?>" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-primary">
     </div>
