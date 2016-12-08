135fd3f13304d3a4c28fab65eec263faf7f7c04d
docker <dock3r@gmail.com>
2016-12-08 16:32:11 +0100

    Finalize popup terms

--- templates/terms-en.tpl.php
+++ templates/terms-en.tpl.php
@@ -1,3 +1,95 @@
-<p>English Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sollicitudin lorem bibendum metus maximus, vitae tincidunt libero venenatis. Nunc suscipit arcu a nunc malesuada cursus. Morbi molestie tempor dolor non suscipit. Curabitur eros lectus, sagittis non justo et, luctus tincidunt eros. Sed vulputate leo non ipsum tincidunt maximus. Praesent efficitur orci id facilisis dapibus. Aliquam sodales ipsum sed lacus interdum, sed vulputate lacus venenatis. Ut faucibus erat magna, eget mattis leo ultrices id. Nam iaculis fermentum auctor. Fusce ornare, eros quis euismod semper, mauris orci luctus est, sed euismod dolor lacus sit amet nisi. Aenean justo ante, fringilla feugiat tellus eu, tristique vestibulum justo. Maecenas id nisi non eros pellentesque faucibus sit amet sit amet orci. Proin gravida blandit odio, sollicitudin venenatis nibh rhoncus sit amet. Phasellus non nibh in orci convallis eleifend in eget ipsum. Nunc ante libero, mollis eget venenatis vel, aliquam quis massa.</p>
+<h4>TERMS AND CONDITIONS FOR DONATIONS THROUGH WWW.TRANSPARENCY.HU</h4>
 
-<p>Aenean porttitor scelerisque ex vitae interdum. Maecenas sodales, tortor id ultrices interdum, mi turpis tincidunt velit, condimentum maximus ex justo eu quam. Sed lacus ipsum, gravida vel elit et, faucibus posuere eros. Sed laoreet in leo non lacinia. Nullam condimentum bibendum facilisis. Suspendisse gravida, tortor et venenatis fermentum, sem tortor dapibus sapien, quis aliquam enim arcu vel neque. Sed pharetra nulla ante, vel viverra ex tristique sed. Suspendisse potenti. Ut augue nulla, interdum non fermentum mollis, congue nec dolor. Pellentesque lectus quam, hendrerit a risus ac, ornare tempus metus. Maecenas a porta nisl. Aliquam id accumsan mauris, in placerat quam. Duis ac leo elit. Nullam pretium, ante nec interdum accumsan, enim magna molestie nisl, eget euismod urna ante non urna.</p>
+<h4>1. Introduction</h4>
+
+<p>1.1. These Terms and Conditions (T&C) shall govern all details pertaining to donation contracts between Transparency International Hungary (TI Hungary; address: 1055 Budapest, Falk Miksa Str. 30. 4F 2., tax number: 18192744-2-41, registration number: 10071, number of registration document: 16 PK 60945 /2006/1., date of registration: 2007.01.11, represented by: József Péter Martin executive director) as the Beneficiary and any Donor who wishes to donate to the Beneficiary from his/her own assets by going through the process presented on the online interface dedicated to this purpose (Beneficiary and Donor together referred to as Parties).</p>
+
+<p>1.2. The long term goal of the Beneficiary is to maintain and operate TI Hungary. The goal of TI Hungary as an independent professional organization is to contribute to mitigating corruption, promoting transparency and accountability in the public sphere of decision making processes as well as allocation of public funds, moreover to improving accessibility of public interest information.</p>
+
+<p>1.3. Beneficiary has created and is maintaining a donation interface (<a href="https://transparency.hu/en/donation/">https://transparency.hu/en/donation/</a>) on its website (www.transparency.hu, referred to as the Website) for the purpose of public fundraising, which forms the framework for the methods of donation detailed in 3.5.</p>
+
+<p>1.4. The Parties hereby accept that the Website is maintained and operated by the Beneficiary who is fully responsible for its operation.</p>
+<h4>2. Formation of the Contract</h4>
+<p>2.1. The formation of the donation contract requires the Donor to donate a certain amount to the Beneficiary from his/her own assets by going through one of the processes presented on the online interface dedicated for this purpose. </p>
+
+<p>2.2. The Donor can express his/her intent to donate to the Beneficiary from his/her own assets by navigating to the Website, choosing from one of the available forms of donation and successfully execute the presented maneuvers (clicking, entering data, etc.). </p>
+
+<p>2.3. After selecting one of the available forms of donation the Donor can read the terms and conditions presented in the current document, and after accepting said terms and conditions the donation contract is formed between the Donor and the Beneficiary without any further legal action necessary, and the Beneficiary simultaneously states that it gratefully accepts the donation.</p>
+<h4>3. The Subject and Content of the Donation Contract</h4>
+<p>3.1. Goal of the donation: the Parties mutually state that the goal of the donation is to provide financial assistance to cover some of the costs incurred for the Beneficiary while working towards achieving its stated goals.</p>
+
+<p>3.2. Levels of Donation</p>
+
+<p>Any amount of donation can be offered to the Beneficiary through the Website, either in the form of a one-time or a regular donation. In order to make the donation process easier the Beneficiary recommends donating the amounts detailed below through the Website. </p>
+
+<p>3.2.1. ’Ally of Transparency’ category. The amount necessary for the first level of donation is HUF 1,000 (one thousand Forints) at least once per calendar year. In case the Donor wishes to support the Beneficiary with said amount the Beneficiary offers the services detailed in 4.4.</p>
+
+
+
+<p>3.2.2. ’Champion of Integrity’ category. The amount necessary for the second level of donation is HUF 5,000 (five thousand Forints) at least once per calendar year. In case the Donor wishes to support the Beneficiary with said amount the Beneficiary offers the services detailed in 4.5.</p>
+
+<p>3.2.3. ’Anti-Corruption Superhero’ category. The amount necessary for the third level of donation is HUF 10,000 (ten thousand Forints) at least once per calendar year. In case the Donor wishes to support the Beneficiary with said amount the Beneficiary offers the services detailed in 4.6.</p>
+
+<p>3.2.4. In addition to the choices offered the Donor can choose to donate a unique amount or in unique intervals. In this case the Donor and the Beneficiary creates a unique agreement for the donation. The Beneficiary will supply information pertaining to the unique agreement upon request from the Donor via the <a href="mailto:https://transparency.hu/en/donation/">info@transparency.hu</a> e-mail address.</p>
+
+<p>3.4. The Donation Process, the Formation of the Contract</p>
+
+<p>3.4.1. In order for someone to become a Donor and a subject of the donation contract detailed in the current document, he/she has to fulfill the following steps: the Donor has to choose from one of the forms of donation detailed in 3.2. and also presented on the Website, after which he/she accepts the current T&C, and finally implements the chosen form of donation through one of the methods detailed in 3.5.</p>
+
+<p>3.4.2. In case the Donor wishes to donate again following the completion of the donation process outlined in 3.4.1. he/she can do so by repeating said process.</p>
+
+<p>3.5. Methods of Payment </p>
+
+<p>3.5.1. Payment can be completed by PayPal.</p>
+
+<p>3.5.2. Payment can be completed by credit card.</p>
+
+<p>3.5.3. Payment can be completed by bank transfer.</p>
+<h4>4. Reciprocity Given to Donors</h4>
+<p>4.1. In order to show its appreciation to the Donor for financially contributing to the operation of TI Hungary, the Beneficiary will provide the services detailed below to the Donor for a period of one year starting from the date the donation is received by the Beneficiary, in accordance with the amount of the donation and the category the Donor falls in, specified by the amount of the donation. </p>
+
+<p>4.2. It is stated that any services described in 4. cannot be viewed as remuneration in exchange for the donation, and are only supplied in order to keep the Donor updated on the work of TI Hungary, and to offer the Donor with the services detailed in 4. The donation is does not qualify as an inland provision of goods or services in exchange for compensation, and so the Beneficiary is exempt from providing a mandatory invoice.</p>
+
+<p>4.3. Any Donor who donates any amount to the Beneficiary in a one year timespan by any of the payment methods described in 3.5. will receive a ‘Donor’ title.</p>
+
+<p>4.3.1. In the situation described in 4.3 the Beneficiary undertakes to send TI Hungary’s Newsletter each month to the Donor, in which the Beneficiary reports in detail on its activities, and supplies information at least once a year on what the Donor’s donation was used for. The Beneficiary sends the Newsletters to the Donor by e-mail to the Donor’s address given upon registration for the Newsletter through the Website.</p>
+
+
+
+
+
+
+
+<p>4.4. Any Donor who donates the at least the amount specified in 3.2.1. to the Beneficiary in a one year timespan by any of the payment methods described in 3.5. will receive the ‘Ally of Transparency’ title.</p>
+
+<p>4.4.1. In this case the Beneficiary undertakes to send all press releases at the moment of their publication, in addition to the monthly Newsletters by e-mail.</p>
+
+<p>4.5. Any Donor who donates at least the amount specified in 3.2.2. to the Beneficiary in a one year timespan by any of the payment methods described in 3.5. will receive the ‘Champion of Integrity’ title.</p>
+
+<p>4.5.1. In this case the Beneficiary undertakes to – in addition to the services described in 4.4.1. – allow the Donor to attend any public event organized by the Beneficiary, and the Donor will receive invitations to the Donor for these events by e-mail. The Parties agree that the events specified can be attended by anyone interested without any entry fee.</p>
+
+<p>4.6. Any Donor who donates at least the amount specified in 3.2.3. to the Beneficiary in a one year timespan by any of the payment methods described in 3.5. will receive the ‘Anti-Corruption Superhero’ title.</p>
+
+<p>4.6.1. In this case the Beneficiary undertakes to – in addition to the services described in 4.5.1. – supply the Donor with a free copy of every publication published by TI Hungary within one year of the date the donation was received, which are sent to the Donor by mail.</p>
+
+<p>4.7. Any Donor whose donation exceeds HUF 15,000 (fifteen thousand Forints) in a period of one calendar year will receive a ‘Transparency International Gift Package’ which will be sent to the Donor by mail. The contents of the Gift Package are the following:</p>
+
+<ul>
+<li>the publications published by TI Hungary, which were presented in the three years prior to the time when the Donor sent its donation to the Beneficiary,</li>
+<li>a T-shirt in the size specified by the Donor, which was manufactured in connection to one of TI Hungary’s projects, and features a logo of TI Hungary,</li>
+<li>a selection of any currently available merchandise branded with the TI Hungary logo.</li>
+</ul>
+
+<h4>5. The Utilization of the Donation</h4>
+<p>The Beneficiary will inform the Donor at least once per calendar year through the Newsletter, and also at the events organized by TI Hungary about how the donations arriving through the Website were utilized. The reporting should adhere to the requirements of transparency.</p>
+<h4>6. Data Management</h4>
+<p>The Parties state that by accepting the T&C the Donor also accepts the contents of 5. § (1) a) of Act CXII of 2011 on Informational Self-determination and Freedom of Information, and so the Beneficiary is entitled to handle any data received pertaining to the Donor which was freely given by the Donor within the stated legal confines. No data will be given out to third parties without the consent of the Donor. The details of data management are contained in the Data Protection Guide which can be accessed on the following link: <a href="https://transparency.hu/en/adatvedelem/">https://transparency.hu/en/adatvedelem/</a> </p>
+<h4>7. Termination of the Donation Contract</h4>
+<p>7.1. The donation contract is terminated with the fulfillment of all its requirements.</p>
+
+<p>7.2. The Parties state that the donation contract can only be terminated prematurely or they can only step back from fulfilling their obligations contained in the contract in case of a serious breach of the contract by either party. </p>
+
+<p>7.3. Among other things the utilization of the donation for purposes contrary to its objectives qualifies as a serious breach of the contract. </p>
+<h4>8. Waiving of the Right to Reclaim the Gifts Given After Donation</h4>
+<p>8.1. The Parties state that the Donor waives his/her rights, bestowed upon him/her by the Hungarian civil law regulations pertaining to donation, to reclaim the gifts given after donation without any further terms or restrictions. </p>
+<h4>9. This agreement is subject to Hungarian Civil Law. In case of issues not governed by this T&C the articles pertaining to gift giving and other general regulations from the Hungarian Civil Code will be applicable.</h4>
